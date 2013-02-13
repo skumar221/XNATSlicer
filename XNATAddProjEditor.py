43171974f9a0e8faf793a5bdd5fb994ddd612b49
unknown <skumar01@NRG205.nrg.mir>
2013-02-13 12:52:29 -0600

    Comment Updating

--- XNATAddProjEditor.py
+++ XNATAddProjEditor.py
@@ -9,51 +9,35 @@ import urllib2
 
 from XNATUtils import *
 
-
-
-#########################################################
-#
-# 
-comment = """
-  XNATAddProjEditor
-
-# TODO : 
-"""
-#
-#########################################################
+#===============================================================================
+# XNATAddProjEditor is used for creating new projects/folders within a given
+# XNAT host.  It manages talking to a given XNATCommunicator and its associated
+# string inputs. 
+#===============================================================================
 
 class XNATAddProjEditor(object):
     def __init__(self, viewer = None, browser = None, XNATCommunicator = None):
         self.browser = browser
         self.viewer = viewer
         self.XNATCommunicator = XNATCommunicator
-               
         self.projLabel = qt.QLabel("Project")
         self.projDD = qt.QComboBox()
         self.projDD.addItems(self.XNATCommunicator.getFolderContents('/projects'))
         self.projDD.connect("currentIndexChanged(const QString&)", self.populateSubjectDD)
         self.projLE = qt.QLineEdit()
         self.projLE.connect("textEdited(const QString&)", self.projLineEdited)
-
-        #projError = qt.QLabel("<font color=\"red\">P Error</font>")
         self.projError = qt.QLabel()
         self.projError.setTextFormat(1)
-        
         self.subjLabel = qt.QLabel("Subject")
         self.subjDD = qt.QComboBox()
         self.subjLE = qt.QLineEdit()
         self.subjLE.connect("textEdited(const QString&)", self.subjLineEdited)
-        #subjError = qt.QLabel("<font color=\"red\">s Error</font>")
         self.subjError = qt.QLabel()
         self.subjError.setTextFormat(1)
-        
         self.exptLabel = qt.QLabel("Experiment")
-        #exptDD = qt.QComboBox()
         self.exptLE = qt.QLineEdit()
-        #exptError = qt.QLabel("<font color=\"red\">e Error</font>")
         self.exptError = qt.QLabel()
         self.exptError.setTextFormat(1)
-
         p = None
         s = None
         proj = ""
@@ -63,10 +47,8 @@ class XNATAddProjEditor(object):
             proj = p.text(self.viewer.column_name)
             self.projDD.setCurrentIndex(self.projDD.findText(proj))
         else:
-            proj = self.projDD.currentText
-        
-        self.populateSubjectDD(proj)
-        
+            proj = self.projDD.currentText       
+        self.populateSubjectDD(proj)        
         if s:
             self.subjDD.setCurrentIndex(self.subjDD.findText(s.text(self.viewer.column_name)))
 
@@ -82,34 +64,25 @@ class XNATAddProjEditor(object):
         l = qt.QGridLayout()
         self.addProjWindow = qt.QWidget()
         self.addProjWindow.setFixedWidth(700)
-        #self.addProjWindow.setWindowModality(2)
-        
         mainWindow = slicer.util.mainWindow()
         screenMainPos = mainWindow.pos
         x = screenMainPos.x() + mainWindow.width/2 - self.addProjWindow.width/2
         y = screenMainPos.y() + mainWindow.height/2 - self.addProjWindow.height/2
-        
         existingCol = qt.QLabel("Existing")
         newCol = qt.QLabel("New")
-
         l.addWidget(existingCol, 0, 1)
         l.addWidget(newCol, 0, 2)
-        
         l.addWidget(self.projLabel, 1, 0)
         l.addWidget(self.projDD, 1, 1)
         l.addWidget(self.projLE, 1, 2)
         l.addWidget(self.projError, 2, 2)
-        
         l.addWidget(self.subjLabel, 3, 0)
         l.addWidget(self.subjDD, 3, 1)
         l.addWidget(self.subjLE, 3, 2)
         l.addWidget(self.subjError, 4, 2)
-        
         l.addWidget(self.exptLabel, 5, 0)
-        #l.addWidget(exptDD, 3, 1)
         l.addWidget(self.exptLE, 5, 2)
         l.addWidget(self.exptError, 6, 2)
-
         #=======================================================================
         # BUTTONS
         #=======================================================================
@@ -121,9 +94,7 @@ class XNATAddProjEditor(object):
         buttonRow.addButton(createButton, 0)
         buttonRow.addButton(cancelButton, 2)
         buttonRow.connect('clicked(QAbstractButton*)', self.createButtonClicked)
-        
         l.addWidget(buttonRow, 7, 2)
-        
         self.addProjWindow.setLayout(l)
         self.addProjWindow.move(qt.QPoint(x,y))
         self.addProjWindow.setWindowTitle("Add Folder to XNAT")
@@ -131,13 +102,16 @@ class XNATAddProjEditor(object):
     
     def createButtonClicked(self,button):
         #=======================================================================
-        # IF OK BUTTON CLICKED
+        # If OK button clicked..,.
         #=======================================================================
         if 'create' in button.text.lower():
             self.exptError.setText("")
             self.subjError.setText("")
             self.projError.setText("")
             pathStr = "/projects/"
+            #===================================================================
+            # STEP 1: Sum up the xnat text boxes according to XNAT file organizational rules
+            #===================================================================
             if len(self.projLE.text)>0:
                 pathStr += self.projLE.text
                 if len(self.subjLE.text)>0:
@@ -154,7 +128,9 @@ class XNATAddProjEditor(object):
                     pathStr += "/subjects/" + self.subjDD.currentText
                     if len(self.exptLE.text)>0:
                         pathStr += "/experiments/" + self.exptLE.text
-
+            #===================================================================
+            # STEP 2: After summing up the text to XNAT folders, see if they exist already
+            #===================================================================
             if (self.XNATCommunicator.fileExists(pathStr)):
                 print ("%s ALREADY EXISTS!"%(pathStr))
                 projStr = pathStr.split("/subjects")[0]
@@ -170,7 +146,9 @@ class XNATAddProjEditor(object):
                     self.subjError.setText("<font color=\"red\">*Subject already exists.</font>")
                 elif (projStr and self.XNATCommunicator.fileExists(projStr)):
                     self.projError.setText("<font color=\"red\">*Project already exists.</font>")
-
+            #===================================================================
+            # STEP 3: Doesn't exist? Then make the folders.
+            #===================================================================
             else:
                 self.XNATCommunicator.makeDir(pathStr)
                 slicer.app.processEvents()
