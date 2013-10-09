cd94fab04a0eac996393ac059572ee8be2736650
Sunil Kumar <sunil.kumar@alumni.duke.edu>
2013-10-09 12:20:35 -0700

    Implemented search, minor bug fixes

--- XnatSlicer/XnatSlicerLib/XnatFileLoadWorkflow.py
+++ XnatSlicer/XnatSlicerLib/XnatFileLoadWorkflow.py
@@ -2,6 +2,12 @@ from XnatLoadWorkflow import *
 
 
 
+comment = """
+XnatFileLoadWorkflow contains the specific load method for single-file
+(non-Slicer scene) downloads from an XNAT host into Slicer.
+
+TODO:
+"""
 
 
 
@@ -9,36 +15,62 @@ class XnatFileLoadWorkflow(XnatLoadWorkflow):
         
     def setup(self):
         pass
+
+
+
     
     def load(self, args):
-        """Iterates through the various types of Slicer nodes by attempting to load fileName
-           and reading whether or not the loadNodeFromFile method returns True or False.  
-           Also updates the UI locking and Status components."""
+        """ Downloads a file from an XNAT host, then attempts to load it
+            via the Slicer API's 'loadNodeFromFile' method, which returns
+            True or False if the load was successful.
+        """
 
-           
-        # Call parent
+        #--------------------    
+        # Call parent 'load' method.
+        #-------------------- 
         super(XnatFileLoadWorkflow, self).load(args)
 
 
-        # Get the file
+        
+        #-------------------- 
+        # Get the file from XNAT host.
+        #-------------------- 
         self.browser.XnatCommunicator.getFile({self.xnatSrc: self.localDst})
         
 
-        # Open file
+
+        #-------------------- 
+        # Attempt to open file
+        #-------------------- 
         a = slicer.app.coreIOManager()
         t = a.fileType(self.localDst)
-        nodeOpener = slicer.util.loadNodeFromFile(self.localDst, t)
+        fileSuccessfullyLoaded = slicer.util.loadNodeFromFile(self.localDst, t)
 
-        
-        # Update status bar
-        sessionArgs = XnatSessionArgs(browser = self.browser, srcPath = self.xnatSrc)
-        sessionArgs['sessionType'] = "scene download"
-        self.browser.XnatView.startNewSession(sessionArgs)
-        
-        if nodeOpener: 
-           print ("'%s' successfully loaded."%(os.path.basename(self.localDst))) 
+
+
+        #-------------------- 
+        # If the load was successful, update the
+        # session args...
+        #-------------------- 
+        if fileSuccessfullyLoaded: 
+            sessionArgs = XnatSessionArgs(browser = self.browser, srcPath = self.xnatSrc)
+            sessionArgs['sessionType'] = "scene download"
+            self.browser.XnatView.startNewSession(sessionArgs)
+            print ("'%s' successfully loaded."%(os.path.basename(self.localDst))) 
+
+
+
+        #-------------------- 
+        # Otherwise kick back error.
+        #-------------------- 
         else: 
             errStr = "Could not load '%s'!"%(os.path.basename(self.localDst))
             print (errStr)
             qt.QMessageBox.warning( None, "Load Failed", errStr) 
-        return nodeOpener
+
+
+            
+        #-------------------- 
+        # Return the load success
+        #-------------------- 
+        return fileSuccessfullyLoaded
